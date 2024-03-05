7c79893f1f8508bbbf1f34b248a1a447c7abc06d
Marcela Trujillo Toro <marces.ttblue@gmail.com>
2024-03-05 16:36:15 -0500

    config: firebase

--- android/app/src/main/kotlin/com/marcelatrujillo/push_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/marcelatrujillo/push_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.push_app
+package com.marcelatrujillo.push_app
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:push_app/firebase_options.dart';
+
+part 'notifications_event.dart';
+part 'notifications_state.dart';
+
+class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationsBloc() : super(const NotificationsState()) {
+    // on<NotificationsEvent>((event, emit) {
+    // });
+  }
+
+  static Future<void> initializeFirebaseNotifications() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
+  void requestPermission() async {
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: true,
+      provisional: false,
+      sound: true,
+    );
+
+    settings.authorizationStatus;
+  }
+}
